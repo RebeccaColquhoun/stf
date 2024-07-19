617b105cfb75265becd8b4af12e4c38dacaddf54
RebeccaColquhoun <rcolquhoun13@gmail.com>
2024-07-19 10:13:51 +0100

    download from isc and put metadata from isc and scardec into pandas dataframes

--- code/downloading/get_from_ISC_bulletin.py
+++ code/downloading/get_from_ISC_bulletin.py
@@ -3,12 +3,19 @@ import html2text
 import obspy
 import pickle
 
-save_path = '/home/earthquakes1/homes/Rebecca/phd/stf/data/isc_test/'
+save_path = '/home/earthquakes1/homes/Rebecca/phd/stf/data/isc/'
 
 download_data = True
 
+cat_params = {'start_year': 2020,
+              'start_month': 1,
+              'start_day': 1,
+              'end_year': 2021,
+              'end_month': 1,
+              'end_day': 1}
 
-def build_link(start_year=2018, start_month=1, start_day=1, end_year=2021, end_month=3, end_day=1, file_type="ISF2"):
+
+def build_link(cat_params, file_type="ISF2"):
 
     link = ("http://www.isc.ac.uk/cgi-bin/web-db-run?"
             f"request=COMPREHENSIVE&out_format={file_type}"
@@ -17,10 +24,14 @@ def build_link(start_year=2018, start_month=1, start_day=1, end_year=2021, end_m
             "&radius=&max_dist_units=deg"
             "&searchshape=GLOBAL"
             "&srn=&grn="
-            f"&start_year={str(start_year)}&start_month={str(start_month).zfill(2)}"
-            f"&start_day={str(start_day).zfill(2)}&start_time=00%3A00%3A00"
-            f"&end_year={str(end_year)}&end_month={str(end_month).zfill(2)}"
-            f"&end_day={str(end_day).zfill(2)}&end_time=00%3A00%3A00"
+            f"&start_year={str(cat_params['start_year'])}"
+            f"&start_month={str(cat_params['start_month']).zfill(2)}"
+            f"&start_day={str(cat_params['start_day']).zfill(2)}"
+            "&start_time=00%3A00%3A00"
+            f"&end_year={str(cat_params['end_year'])}"
+            f"&end_month={str(cat_params['end_month']).zfill(2)}"
+            f"&end_day={str(cat_params['end_day']).zfill(2)}"
+            "&end_time=00%3A00%3A00"
             "&min_dep=&max_dep="
             "&min_mag=5.5&max_mag="
             "&req_mag_type=&req_mag_agcy=GCMT"
@@ -32,16 +43,16 @@ def build_link(start_year=2018, start_month=1, start_day=1, end_year=2021, end_m
     return link
 
 
-def read_bulletin_from_web_and_save():
-    link = build_link(file_type="ISF2")
+def read_bulletin_from_web_and_save(cat_params):
+    link = build_link(cat_params, file_type="ISF2")
     f = urllib.request.urlretrieve(link, f'{save_path}initial_content.txt')
-    j = open('initial_content.txt')
+    j = open(f'{save_path}initial_content.txt')
     k = j.read()
     rendered_content = html2text.html2text(k)
     j.close()
-    with open('rendered_content.txt', 'w') as f:
+    with open(f'{save_path}rendered_content.txt', 'w') as f:
         f.write(rendered_content)
-    cat_link = build_link(file_type='QuakeML')
+    cat_link = build_link(cat_params, file_type='QuakeML')
     urllib.request.urlretrieve(cat_link, f'{save_path}quakeml.xml')
     print('bulletin downloaded')
 
@@ -71,6 +82,7 @@ def look_for_stf(split_at_new_lines, s):
                      'NST': int(values[4]),
                      'NWV': int(values[5]),
                      'Author': values[6]}
+        print(norm_dict, 'about to return')
         return norm_dict
     else:
         return None
@@ -82,16 +94,24 @@ def look_for_norm_stf(split_at_new_lines, s, stf_exists):
         print("found NORMALISED_STF")
         stf_exists = True
         stf = split_at_new_lines[s + 1:s + 33]
+        #print(stf)
         for line in stf:
             for item in line.split(' '):
+                #print('item', item)
                 if item not in ['', '#', '(#', ')']:
                     stf_list.append(float(item))
+                    #print('stflist', stf_list)
     return stf_exists, stf_list
 
 
-def look_for_event_in_catalog_and_save(count, quakeml, stf_list, norm_dict):
+def look_for_event_in_catalog_and_save(eq_id,
+                                       count,
+                                       quakeml,
+                                       stf_list,
+                                       norm_dict):
     for event in quakeml:
         print('eventid', event.resource_id.id[-9:])
+        print(eq_id, event.resource_id.id[-9:])
         if event.resource_id.id[-9:] == eq_id:
             print('same')
 
@@ -103,39 +123,55 @@ def look_for_event_in_catalog_and_save(count, quakeml, stf_list, norm_dict):
                 pickle.dump(norm_dict, f)
             print('saved')
             count += 1
+            break
     return count
 
 
-print('running')
+def ISC_download_and_process_data(cat_params, download_data=True):
+    print('running')
 
-if download_data is True:
-    read_bulletin_from_web_and_save()
-    print('downloaded')
+    if download_data is True:
+        read_bulletin_from_web_and_save(cat_params)
+        print('downloaded')
 
-r, quakeml = read_from_file()
-events_split = split_bulletin_into_events(r)
-print('consider each event in turn')
-count = 0
+    r, quakeml = read_from_file()
+    events_split = split_bulletin_into_events(r)
+    print('consider each event in turn')
+    count = 0
 
-for i in range(1, len(events_split)):
 
-    eq_id = events_split[i].split(']')[0]
-    print(eq_id, "+++")
+    for i in range(1, len(events_split)):
 
-    split_at_new_lines = events_split[i].split('\n')
-    stf_exists = False
-    for s in range(0, len(split_at_new_lines)):
-        stf_exists, stf_list = look_for_norm_stf(split_at_new_lines,
-                                                 s,
-                                                 stf_exists)
-        norm_dict = look_for_stf(split_at_new_lines, s)
-        print(stf_exists)
+        eq_id = events_split[i].split(']')[0]
+        print(eq_id, "+++")
 
-    if stf_exists is True:
-        print('stf exists')
-        count = look_for_event_in_catalog_and_save(count,
-                                                   quakeml,
-                                                   stf_list,
-                                                   norm_dict)
+        split_at_new_lines = events_split[i].split('\n')
+        #print(split_at_new_lines)
+        stf_exists = False
+        norm_dict = None
+        for s in range(0, len(split_at_new_lines)):
+            if stf_exists is False:
+                stf_exists, stf_list = look_for_norm_stf(split_at_new_lines,
+                                                     s,
+                                                     stf_exists)
+                print(stf_exists)
+            if norm_dict is None:
+                norm_dict = look_for_stf(split_at_new_lines, s)
+                print(norm_dict)
 
-print(count)
+        if stf_exists is True:
+            print('stf exists')
+            count = look_for_event_in_catalog_and_save(eq_id,
+                                                       count,
+                                                       quakeml,
+                                                       stf_list,
+                                                       norm_dict)
+            break
+
+    print(count)
+
+for i in range(2016, 2024):
+    cat_params['start_year'] = i
+    cat_params['end_year'] = i + 1
+
+    ISC_download_and_process_data(cat_params, download_data=True)
